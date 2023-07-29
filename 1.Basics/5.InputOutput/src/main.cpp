c96eb6c91dbe8473732ead8d092d282f6037fe4b
AZen <418130444@qq.com>
2023-07-29 18:07:26 +0800

    update

--- 1.Basics/5.InputOutput/src/main.cpp
+++ 1.Basics/5.InputOutput/src/main.cpp
@@ -1,25 +1,8 @@
 #include <iostream>
-#include <fstream>
+
 using namespace std;
 
-void load_mnist_labels(const string& file){
-
-    ifstream in(file, ios::binary | ios::in);
-
-    if(!in.is_open()){
-        printf("Open File Failed!");
-    }
-
-    int header[2];
-    in.read((char*)header, sizeof(header));
-    cout << header[0] << endl;
-
-}
-
-int main()
+int main ()
 {
-    string labels = "/datav/MyLesson/2_Cpp/3_CC++_Programing_Basics/5_InputOutput/workspace/t10k-labels-idx1-ubyte";
-    load_mnist_labels(labels);
 
-    return 0;
 }
