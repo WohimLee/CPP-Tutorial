00a469cebe3235c9c56eaa91df08b5e434a467e5
AZen <418130444@qq.com>
2023-08-06 16:27:41 +0800

    update

--- 1.Basics/6.DataTypesAdvanced/1.Arrays/src/main.cpp
+++ 1.Basics/6.DataTypesAdvanced/1.Arrays/src/main.cpp
@@ -2,17 +2,24 @@
 #include <iostream>
 
 int main() {
-    char str1[] = {'H', 'e','l','l','o','\0'};
-    char str2[] = {'H', 'e','l','l','o'};
-    char str3[] = "Hello"; // 字符串常量初始化，自动加 \0
+    int var[3][3][3];
+    int count = 50;
+    for(int i=0; i< 3; i++){
+        for(int j=0; j<3; j++){
+            for(int k=0; k<3; k++){
+                var[i][j][k]=count;
+                count++;
+            }
+        }
+    }
+    printf("Address of var: %p.\n", &var);
+    printf("Value of var: %p.\n", var);
 
-    std::cout << str1 << std::endl;
-    std::cout << str2 << std::endl; // 没有结束符，会一直打印内存后面接着的char 
-    std::cout << str3 << std::endl;
+    for(int i=0; i<3; i++){
+        printf("Value of var[%d]: %p.\n", i, var[i]);
+    }
 
-    printf("str1: %s\n", str1);
-    printf("str2: %s\n", str2);
-    printf("str3: %s\n", str3);
+
 
     return 0;
 }
