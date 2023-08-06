b33e846d9d55a3bfd43d228100d4a68c56b89c76
AZen <418130444@qq.com>
2023-08-07 00:20:53 +0800

    update

--- 1.Basics/6.DataTypesAdvanced/2.Pointers/src/main.cpp
+++ 1.Basics/6.DataTypesAdvanced/2.Pointers/src/main.cpp
@@ -1,30 +1,29 @@
 #include <iostream>
 using namespace std;
 
-int addition(int a, int b)
-{
-    return a + b;
-}
-
-int substraction(int a, int b)
-{
-    return a - b;
-}
-
-int(*myfunc)(int, int) = substraction;
-
-int operation(int x, int y, int(*functocall)(int, int))
-{
-    return ((*functocall)(x, y));
-}
 
 int main()
 {
-    int m, n;
-    m = operation(7, 5, addition);
-    n = operation(20, 10, myfunc);
-    cout << m << endl;
-    cout << n << endl;
+    int a  = 10;
+    char b = 'x';
+
+    int* pInt   = &a;
+    char* pChar = &b;
+
+    // 增加 4 (字节数)
+    printf("%-17s: %p\n", "value of pInt", pInt);
+    printf("%-17s: %p\n", "value of pInt+=1", pInt+=1);
+    printf("%-17s: %p\n", "value of ++pInt", ++pInt);
+    printf("%-17s: %p\n", "value of pInt+1", pInt+1); // 不推荐
+
+    printf("\n");
+
+    // 减少 1 (字节数)
+    printf("%-18s: %p\n", "value of pChar", pChar);
+    printf("%-18s: %p\n", "value of pChar+=1", pChar-=1);
+    printf("%-18s: %p\n", "value of ++pCnt", --pChar);
+    printf("%-18s: %p\n", "value of pCnt+1", pChar-1); // 不推荐
+
 
     return 0;
 }
