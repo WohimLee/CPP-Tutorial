17ac94337a3efd9996e7504f2e8372a767ad71ea
AZen <418130444@qq.com>
2023-08-07 14:23:50 +0800

    update

--- 1.Basics/6.DataTypesAdvanced/1.Arrays/src/main.cpp
+++ 1.Basics/6.DataTypesAdvanced/1.Arrays/src/main.cpp
@@ -1,18 +1,15 @@
-#include <stdio.h>
-#include <iostream>
 
-using namespace std;
+#include <stdio.h>
 
 int main()
 {
     int a[3][2][2] = {0,1,2,3,4,5,6,7,8,9,10,11};
-    // address of block 2, row 0; point to 2 int
-    printf("%s: %p\n", "value of a[2][0]", a[2][0]);
-    printf("%s: %d\n\n", "value of a[2][0][0]", a[2][0][0]);
+    // 相当于索引 11 = 2*(2*2) + 1*1 + 1
+    printf("value of a[2][1][1]: %d\n", a[2][1][1]);
 
-    // same address; point to 1 int
-    int *ptr = (int*)a[2][0];
-    printf("value of ptr: %p\n", ptr); 
-    printf("value pointed by ptr: %d\n", *ptr);
+    // printf("value of a[11]: %d\n", a[11]); // 有问题, 不是想要的数字, 指针讲解
+    int* ptr = (int*)a;
+    printf("value of ptr[11]   : %d\n", ptr[11]);
+
     return 0;
 }
