9dfea0f323c39c5ffdb1b0e705d4cb1eb6c461eb
AZen <418130444@qq.com>
2023-08-07 11:09:02 +0800

    update

--- 1.Basics/5.FlowControl/src/main.cpp
+++ 1.Basics/5.FlowControl/src/main.cpp
@@ -1,27 +1,9 @@
 
 #include <stdio.h>
-#include <iostream>
 
-using namespace std;
 
-int main ()
+int main()
 {
-  char choice;
-  cout << "Enter A, B or C: ";
-  cin >> choice;
-  switch(choice)
-  {
-    case 'A':
-      cout << "You entered A." << endl;
-      break;
-    case 'B':
-      cout << "You entered B." << endl;
-      break;
-    case 'C':
-      cout << "You entered C." << endl;
-      break;
-    default:
-      cout << "not A, B or C." << endl;
-  }
-  return 0;
-} 
+
+    return 0;
+}
--- 1.Basics/6.DataTypesAdvanced/2.Pointers/src/main.cpp
+++ 1.Basics/6.DataTypesAdvanced/2.Pointers/src/main.cpp
@@ -1,29 +1,9 @@
-#include <iostream>
-using namespace std;
-
+#include <stdio.h>
 
 int main()
 {
-    int a  = 10;
-    char b = 'x';
-
-    int* pInt   = &a;
-    char* pChar = &b;
-
-    // 增加 4 (字节数)
-    printf("%-17s: %p\n", "value of pInt", pInt);
-    printf("%-17s: %p\n", "value of pInt+=1", pInt+=1);
-    printf("%-17s: %p\n", "value of ++pInt", ++pInt);
-    printf("%-17s: %p\n", "value of pInt+1", pInt+1); // 不推荐
-
-    printf("\n");
-
-    // 减少 1 (字节数)
-    printf("%-18s: %p\n", "value of pChar", pChar);
-    printf("%-18s: %p\n", "value of pChar+=1", pChar-=1);
-    printf("%-18s: %p\n", "value of ++pCnt", --pChar);
-    printf("%-18s: %p\n", "value of pCnt+1", pChar-1); // 不推荐
-
+    int a[3][2][2] = {0,1,2,3,4,5,6,7,8,9,10,11};
 
     return 0;
 }
+
