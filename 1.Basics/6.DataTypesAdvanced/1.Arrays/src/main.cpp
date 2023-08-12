5dbea6f7fad5103b4b1da786859e6f06c7e94ac2
AZen <418130444@qq.com>
2023-08-12 20:32:16 +0800

    update

--- 1.Basics/6.DataTypesAdvanced/1.Arrays/src/main.cpp
+++ 1.Basics/6.DataTypesAdvanced/1.Arrays/src/main.cpp
@@ -1,21 +1,17 @@
 #include <stdio.h>
 #include <iostream>
+#include <cmath>
 
 int main() {
   
-    int aaabbb[3][2][2] = {0,1,2,3,4,5,6,7,8,9,10,11};
-    // int* ptr = (int*)array;
-    // printf("value of ptr[11]: %d\n", ptr[11]);
-    for (int i=0; i<3; i++){
-        for (int j=0; j<2; j++){
-            for (int k=0; k<2; k++)
-            {
-                printf("%d ", aaabbb[i][j][k]);
-            }
-        }
-    }
-    printf("\n");
+    float v[3] = {1, 2, 3};
+    // float w[3] = {1, 2, 3};
+    float u[3];
+    float sum=0;
 
-
+    for(int i=0; i<3; i++)
+        sum += v[i]*v[i];
+    
+    printf("res: %f\n", sqrt(sum));
     return 0;
 }
