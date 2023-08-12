fea309e56d1bbd16259bb7f000fba25d2ceb7341
AZen <418130444@qq.com>
2023-08-12 20:05:35 +0800

    update

--- 1.Basics/6.DataTypesAdvanced/1.Arrays/src/main.cpp
+++ 1.Basics/6.DataTypesAdvanced/1.Arrays/src/main.cpp
@@ -2,17 +2,20 @@
 #include <iostream>
 
 int main() {
-    char str1[] = {'H', 'e','l','l','o','\0'};
-    char str2[] = {'H', 'e','l','l','o'}; // 有的编译器或者C++版本中，如果没有 `\0`, 则会一直打印后面的字符
-    char str3[] = "Hello"; // 字符串常量初始化，自动加 \0
+  
+    int aaabbb[3][2][2] = {0,1,2,3,4,5,6,7,8,9,10,11};
+    // int* ptr = (int*)array;
+    // printf("value of ptr[11]: %d\n", ptr[11]);
+    for (int i=0; i<3; i++){
+        for (int j=0; j<2; j++){
+            for (int k=0; k<2; k++)
+            {
+                printf("%d ", aaabbb[i][j][k]);
+            }
+        }
+    }
+    printf("\n");
 
-    std::cout << str1 << std::endl;
-    std::cout << str2 << std::endl; 
-    std::cout << str3 << std::endl;
-
-    printf("str1: %s\n", str1);
-    printf("str2: %s\n", str2);
-    printf("str3: %s\n", str3);
 
     return 0;
 }
