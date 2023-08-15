137dede149bf816e0001ba53f128e63343ed9a3b
AZen <418130444@qq.com>
2023-08-15 10:58:31 +0800

    update

--- 1.Basics/6.DataTypesAdvanced/3.Struct/src/main.cpp
+++ 1.Basics/6.DataTypesAdvanced/3.Struct/src/main.cpp
@@ -1,26 +1,32 @@
 #include <stdio.h>
+#include <stdlib.h>
 
-
-struct resolution_t{
-    unsigned int width;
-    unsigned int height;
+struct Camera{
+    double focal=500;
+    double fx;
+    double fy;
+    double cx;
+    double cy;
 };
-
-
-struct Frame{
-    resolution_t res;
-    unsigned int id;
-    unsigned char* data;
-};
-
 
 int main()
 {
-    int* a;
-    Frame* pFrame;
-    pFrame->res.width  = 800;
-    pFrame->res.height = 800;
+    // Camera* pCam = new Camera;
+    Camera* pCam;
+    pCam = (struct Camera*)malloc(sizeof(struct Camera));
+    if(pCam == NULL) return -1;
 
+    pCam->fx = 500;
+    pCam->fy = 500;
+    (*pCam).cx = 320;
+    (*pCam).cy = 240;
 
+    printf("cam1.fx = %lf\n", pCam->fx);
+    printf("cam1.fy = %lf\n", pCam->fy);
+    printf("cam1.cx = %lf\n", pCam->cx);
+    printf("cam1.cy = %lf\n", pCam->cy);
+
+    // delete pCam;
+    free(pCam);
     return 0;
 }
