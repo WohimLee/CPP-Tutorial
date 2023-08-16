8cd694955786129fe51987a1c1274d9098cdd127
AZen <418130444@qq.com>
2023-08-16 13:09:10 +0800

    update

--- 1.Basics/8.Advanced/1.MemoryManagement/Examples/main.cpp
+++ 1.Basics/8.Advanced/1.MemoryManagement/Examples/main.cpp
@@ -1,42 +1,27 @@
 #include <stdlib.h>
 #include <stdio.h>
+#include <string.h>
 
-#define OVERFLOW  -2
-#define UNDERFLOW -3
-#define ERROR 0
-
-typedef int cudaError_t;
-
-cudaError_t normMalloc(void* pDev, size_t size)
-{
-    // 内部做了:void* pDev = ptr
-    pDev = (int*)malloc(size); // 并没有改变外面的 ptr
-    if (!pDev)
-        exit(OVERFLOW);
-    return 0;
-}
-
-cudaError_t cudaMalloc(void** ppDev, size_t size)
-{
-    // 内部做了: void** ppDev = (void**)&ptr
-    
-    *ppDev = malloc(10); // *ppDev = ptr 改变了外面的 ptr
-    if (!(*ppDev))
-        exit(OVERFLOW);
-    return 0;
+void printArray(int n, int* ptr){
+    for(int i=0; i<n; i++)
+        printf("%d ", ptr[i]);
+    printf("\n");
 }
 
 int main()
 {
-    int* ptr = NULL;
+    int pSrc[] = {1, 2, 3, 4, 5};
 
-    normMalloc(ptr, sizeof(int));
-    if(ptr) printf("normalMalloc succeed!\n");
-    else printf("normalMalloc faled!\n");
+    int* pDst1 = (int*)malloc(5*sizeof(int));
+    memcpy(pDst1, pSrc, 3*sizeof(int));
+    printArray(3, pDst1);
 
-    // cudaMalloc((void**)&ptr, sizeof(int));
-    // if(ptr) printf("cudaMalloc succeed!\n");
-    // else printf("cudaMalloc faled!\n");
+    int* pDst2 = (int*)memcpy(pDst1, pSrc, 3*sizeof(int));
+    printArray(3, pDst2);
+    printf("value of pDst1: %p\n", pDst1);
+    printf("value of pDst2: %p\n", pDst2);
 
+    free(pDst1);
+    pDst1 = NULL;
     return 0;
 }
