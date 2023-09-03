416de6f514f9762a73f86d3c00374377e19d57ff
AZen <418130444@qq.com>
2023-09-03 19:35:33 +0800

    update

--- 1.Basics/9.OOP/2.Basics/src/main.cpp
+++ 1.Basics/9.OOP/2.Basics/src/main.cpp
@@ -1,78 +1,43 @@
-
 #include <iostream>
 using namespace std;
 
-#define INIT_SIZE 128
+class Person;
 
-#ifndef _MATH_H_
-#define OVERFLOW  -2
-#define UNDERFLOW -3
-#endif // _MATH_H_
-
-class List
+class Police
 {
 public :
-    ~List();
-    List();
-    List(int n);
-    // size 加了 const 重载
-    int size();
-    int size() const;
-    // capacity 没加 const 重载
-    int capacity();
-
-private :
-    void* m_data = nullptr;
-    int m_size;
-    int m_capacity;
+    Police(){};
+    void getinfo(Person* person);
 };
 
-List::~List(){
-    free(m_data);
-    m_size = 0;
-    m_capacity = 0;
-}
-
-
-List::List(){
-    m_data = malloc(INIT_SIZE*sizeof(int));
-    if (!m_data)
-        exit(OVERFLOW);
-    m_size = 0;
-    m_capacity = INIT_SIZE;
-}
-
-List::List(int n)
+class Person
 {
-    m_data = (int*)malloc(n*sizeof(int));
-    if (!m_data)
-        exit(OVERFLOW);
-    m_size = 0;
-    m_capacity = n;
+    // friend class Police;
+    friend void Police::getinfo(Person* person);
+public:
+    Person(){};
+    Person(int age, long deposit);
+public :
+    int age;
+private :
+    long deposit;
+};
+
+Person::Person(int _age, long _deposit)
+{
+    this->age = _age;
+    this->deposit = _deposit;
 }
 
-int List::size(){
-    return this->m_size;
+void Police::getinfo(Person* person)
+{
+    cout << person->age << endl;
+    cout << person->deposit << endl;
 }
 
-int List::size() const{
-    return this->m_size;
-}
-
-int List::capacity(){
-    return this->m_capacity;
-}
-
-int main(){
-    const List L1; // 加了 const 修饰的实例
-          List L2; // 没有加 const 修饰的实例
-    // size()有重载，常对象L1可以调用size()
-    cout << L1.size() << endl;
-    cout << L2.size() << endl;
-
-    // capacity()没有重载，常对象L1无法调用capacity()
-    // cout << L1.capacity() << endl; 
-    cout << L2.capacity() << endl;
-
-    return 0;
+int main()
+{
+    Police police;
+    Person man(34, 190000);
+    police.getinfo(&man);
 }
