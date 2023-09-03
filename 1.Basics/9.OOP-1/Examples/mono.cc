1d3fecaf34684a39e5c84ce62a1abc6be1e8125e
AZen <418130444@qq.com>
2023-09-03 09:45:39 +0800

    update

--- 1.Basics/9.OOP-1/Examples/mono.cc
+++ 1.Basics/9.OOP-1/Examples/mono.cc
@@ -1,10 +1,32 @@
 
 #include <iostream>
-#include "Map.h"
+
+
+class Person
+{
+public:
+    int* m_age;
+    int* m_id;
+
+    Person(int age, int id){
+        printf("Call constructor: Person(int age, int id)\n");
+        m_age = new int;
+        m_id =  new int;
+        *m_age = age;
+        *m_id = id;
+    }
+
+    ~Person(){
+        printf("Call destructor: ~Person()\n");
+        delete m_age;
+        delete m_id;
+    }
+};
 
 int main(int argc, char** argv)
 {
-    ORB_SLAM2::Map mapper;
+    Person* p = new Person(20, 888888);
+    delete p;
 
     return 0;
 }
