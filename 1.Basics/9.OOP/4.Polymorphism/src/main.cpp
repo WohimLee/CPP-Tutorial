666c679f4fa820bd3a1ea35e71d4c799cddaece1
AZen <418130444@qq.com>
2023-09-06 00:31:20 +0800

    update

--- 1.Basics/9.OOP/4.Polymorphism/src/main.cpp
+++ 1.Basics/9.OOP/4.Polymorphism/src/main.cpp
@@ -1,115 +1,29 @@
+
 #include <iostream>
-#include <string>
 
-using namespace std;
-
-
-class Person
-{
-    public :   
-        virtual ~Person() = 0;
-        Person(); 
-
-        virtual void BuyTicket(int num);      
-};
-
-class Adult : public Person
-{
-    public :
-        ~Adult();
-        Adult(string name);
-
-        void BuyTicket(int num);
-    private :
-        string* m_name;
-};
-
-class Student : public Person
-{
-    public :
-        ~Student();
-        Student(string name);
-
-        void BuyTicket(int num);
-    private :
-        string* m_name;
-};
-
-Person::Person(){cout << "Person构造函数" << endl;}
-Person::~Person(){cout << "~Person析构函数" << endl;}
-void Person::BuyTicket(int _num){cout << "乘客请买票" << endl;}
-
-Adult::Adult(string _name){
-    cout << "Adult构造函数" << endl;
-    m_name = new string(_name);
-}
-Adult::~Adult(){
-    if (m_name != NULL){
-        cout << "~Adult析构函数" << endl;
-    delete m_name;
-    m_name = NULL;
+class Calculator {
+public:
+    int add(int a, int b) {
+        printf("Call function: int add(int a, int b);\n");
+        return a + b;
     }
-}
-void Adult::BuyTicket(int _num){
-    cout << "成人全票" << endl;
-}
 
-Student::Student(string _name){
-    cout << "Student构造函数" << endl;
-    m_name = new string(_name);
-}
-Student::~Student(){
-    if (m_name != NULL){
-        cout << "~Student析构函数" << endl;
-    delete m_name;
-    m_name = NULL;
+    double add(double a, double b) {
+        printf("Call function: double add(double a, double b);\n");
+        return a + b;
     }
-}
-void Student::BuyTicket(int _num){
-    cout << "学生半票" << endl;
-}
 
+    int add(int a, int b, int c) {
+        printf("Call function: int add(int a, int b, int c);\n");
+        return a + b + c;
+    }
+};
 
-void buy(Person* obj){
-    obj->BuyTicket(1);
-}
-
-int main()
-{
-    Person* p1 = new Adult("Papa");
-    buy(p1);
-    delete p1;
-
-    // Student* s = new Student("Son");
+int main() {
+    Calculator calculator;
+    calculator.add(1, 2);     // Calls the first add method
+    calculator.add(1.1, 2.2); // Calls the second add method
+    calculator.add(1, 2, 3);  // Calls the third add method
 
     return 0;
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
