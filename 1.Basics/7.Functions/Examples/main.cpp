c298c9bf0bf6229f80002b20961cc32c23ef41d7
AZen <418130444@qq.com>
2023-09-05 15:25:39 +0800

    update

--- 1.Basics/7.Functions/Examples/main.cpp
+++ 1.Basics/7.Functions/Examples/main.cpp
@@ -1,40 +1,15 @@
 #include <stdlib.h>
 #include <stdio.h>
 
-#define OVERFLOW  -2
-#define UNDERFLOW -3
-#define ERROR 0
-
-typedef int cudaError_t;
-
-int* Malloc1(void* pDev, size_t size)
-{
-
-    pDev = (int*)malloc(size); // 并没有改变外面的 ptr
-    if (!pDev)
-        exit(OVERFLOW);
-    return (int*)pDev;
-}
-
-int* Malloc2(void* pDev, size_t size)
-{
-
-    pDev = (int*)malloc(size); // 并没有改变外面的 ptr
-    if (!pDev)
-        exit(OVERFLOW);
-    return (int*)pDev;
-}
+struct frame{
+    double matrix[4][4];
+    double a[4][4];
+};
 
 
 int main()
 {
-    int* ptr = NULL;
-
-    ptr = normMalloc(ptr, sizeof(int));
-    if(ptr) printf("normalMalloc succeed!\n");
-    else printf("normalMalloc faled!\n");
-
-    printf("ptr=%p\n", ptr);
 
     return 0;
 }
+
