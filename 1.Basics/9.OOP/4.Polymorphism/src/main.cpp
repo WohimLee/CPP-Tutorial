14379a6163cbd9ab83a082d79f83c3cfc122a758
AZen <418130444@qq.com>
2023-09-07 13:39:12 +0800

    update

--- 1.Basics/9.OOP/4.Polymorphism/src/main.cpp
+++ 1.Basics/9.OOP/4.Polymorphism/src/main.cpp
@@ -1,29 +1,40 @@
-
 #include <iostream>
 
-class Calculator {
+class Vector2D 
+{
 public:
-    int add(int a, int b) {
-        printf("Call function: int add(int a, int b);\n");
-        return a + b;
+    Vector2D(float x = 0, float y = 0) : x(x), y(y) {}
+
+    Vector2D& operator+=(const Vector2D& other) {
+        this->x += other.x;
+        this->y += other.y;
+        return *this;
     }
 
-    double add(double a, double b) {
-        printf("Call function: double add(double a, double b);\n");
-        return a + b;
+    Vector2D operator+(const Vector2D& other) const {
+        return Vector2D(this->x + other.x, this->y + other.y);
     }
 
-    int add(int a, int b, int c) {
-        printf("Call function: int add(int a, int b, int c);\n");
-        return a + b + c;
+    void data() const {
+        printf("x=%-9f, y=%f\n", x, y);
     }
+
+private:
+    float x, y;
 };
 
 int main() {
-    Calculator calculator;
-    calculator.add(1, 2);     // Calls the first add method
-    calculator.add(1.1, 2.2); // Calls the second add method
-    calculator.add(1, 2, 3);  // Calls the third add method
+    Vector2D vec1(1.0f, 2.0f);
+    Vector2D vec2(3.0f, 4.0f);
+    Vector2D vec3(5.0f, 7.0f);
+    
+    vec1 += vec2;
+    printf("vec1: ");
+    vec1.data(); // Output: X: 4.0, Y: 6.0
+
+    vec1 += vec2 += vec3;
+    printf("vec1: ");
+    vec1.data(); // Output: X: 4.0, Y: 6.0
 
     return 0;
 }
