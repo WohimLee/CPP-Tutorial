0b8173ef118db556e35daca5c7f87aad7a98447e
AZen <418130444@qq.com>
2023-09-08 11:47:23 +0800

    update

--- 1.Basics/9.OOP/4.Polymorphism/src/main.cpp
+++ 1.Basics/9.OOP/4.Polymorphism/src/main.cpp
@@ -1,40 +1,33 @@
 #include <iostream>
 
-class Vector2D 
+class Person 
 {
 public:
-    Vector2D(float x = 0, float y = 0) : x(x), y(y) {}
-
-    Vector2D& operator+=(const Vector2D& other) {
-        this->x += other.x;
-        this->y += other.y;
-        return *this;
+    Person(){
+        printf("Call constructor: Person::Person()\n");
     }
-
-    Vector2D operator+(const Vector2D& other) const {
-        return Vector2D(this->x + other.x, this->y + other.y);
-    }
-
-    void data() const {
-        printf("x=%-9f, y=%f\n", x, y);
-    }
-
-private:
-    float x, y;
+    virtual ~Person() = 0;
 };
 
-int main() {
-    Vector2D vec1(1.0f, 2.0f);
-    Vector2D vec2(3.0f, 4.0f);
-    Vector2D vec3(5.0f, 7.0f);
-    
-    vec1 += vec2;
-    printf("vec1: ");
-    vec1.data(); // Output: X: 4.0, Y: 6.0
+Person::~Person(){
+    printf("Call destructor: Person::Person()\n");
+}
 
-    vec1 += vec2 += vec3;
-    printf("vec1: ");
-    vec1.data(); // Output: X: 4.0, Y: 6.0
+class Adult : public Person
+{
+public :
+    Adult(){
+        printf("Call constructor: Adult::Adult()\n");
+    };
+    ~Adult(){
+        printf("Call destructor: Adult::~Adult()\n");
+    };
+};
 
+int main(void)
+{
+    Person* a = new Adult();
+
+    delete a; 
     return 0;
 }
