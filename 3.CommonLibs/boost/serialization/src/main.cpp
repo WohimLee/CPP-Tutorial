53d7e4f6fcf1ac9d276447cb84fe49fcfcafe21f
AZen <418130444@qq.com>
2023-10-18 21:00:45 +0800

    update

--- 3.CommonLibs/boost/serialization/src/main.cpp
+++ 3.CommonLibs/boost/serialization/src/main.cpp
@@ -8,46 +8,63 @@
 #include <boost/archive/binary_oarchive.hpp>
 
 
-class MyClass
+class Base
 {
 public:
-	MyClass():mTag(0), mText(""){}
-	MyClass(int tag, std::string text):mTag(tag), mText(text){}
+	Base():mTag(0), mText(""){}
+	Base(int tag, std::string text):mTag(tag), mText(text){}
  
 	int mTag;
 	std::string mText;
 };
 
-namespace boost {
+class Sub : public Base
+{
+public:
+	Sub():mNum(0.0){}
+	Sub(int tag, std::string text, float num):Base(tag, text), mNum(num){}
+
+	float mNum;
+};
+
+
+namespace boost{
 namespace serialization
 {
-		template<class Archive>
-		void serialize(Archive & ar, MyClass & obj, const unsigned int version)
-		{
-			ar & obj.mTag;
-			ar & obj.mText;
-		}
-} // namespace serialization
-} // namespace boost
-
+	template<class Archive>
+	void serialize(Archive & ar, Base & obj, const unsigned int version)
+	{
+		ar & obj.mTag;
+		ar & obj.mText;
+	}
+	template<class Archive>
+	void serialize(Archive & ar, Sub & obj, const unsigned int version)
+	{
+		// serialize base class information
+		ar & boost::serialization::base_object<Base>(obj);
+		ar & obj.mNum;
+	}
+} // serialization
+} // boost
 
 int main(int argc, char** argv)
 {
     // 序列化
-    MyClass obj1(2023, "Hello!");
+    Sub obj1(2023, "Non-Intrusive Sub class", 3.14);
 	std::ostringstream os;
 	boost::archive::binary_oarchive oa(os);
 	oa << obj1; // 序列化到一个 ostringstream 里面
  
-	std::string content = os.str(); //content保存了序列化后的数据
+	std::string content = os.str(); // content 保存了序列化后的数据
     
     // 反序列化
-	MyClass obj2;
+	Sub obj2;
 	std::istringstream is(content);
 	boost::archive::binary_iarchive ia(is);
 	ia >> obj2; // 从一个保存序列化数据的 string 里面反序列化，从而得到原来的对象。
  
-	printf("obj2.mTag: %d, obj2.mText: %s\n", obj2.mTag, obj2.mText.c_str());
+	printf("Sub object: mTag: %d, obj2.mText: %s, mNum: %f\n", 
+			obj2.mTag, obj2.mText.c_str(), obj2.mNum);
 
     return 0;
 }
